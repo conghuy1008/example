b1eaa02517aca57319cf3536dd0e1c1dce35c65a
conghuy1008@gmail.com <conghuy1008@gmail.com>
2019-01-02 16:20:47 +0700

    Doc Model

---
+++ db/migrate/20190102091446_create_docs.rb
@@ -0,0 +1,11 @@
+class CreateDocs < ActiveRecord::Migration[5.1]
+  def change
+    create_table :docs do |t|
+      t.string :title
+      t.string :content
+      t.string :text
+
+      t.timestamps
+    end
+  end
+end
