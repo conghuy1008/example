53bcccb953fdd49cd0aa19a3d2cfeae2a119fc94
conghuy1008@gmail.com <conghuy1008@gmail.com>
2019-01-03 14:18:28 +0700

    Edit and Destroy Doc

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  get 'docs/index'
 
-  root 'welcome#index'
+  root 'docs#index'
   resources :docs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
